d0a287524177fb19e6f4b9045cc7420c166f5aa1
Yorley <yorleymaria@outlook.com>
2018-05-27 19:06:55 -0600

    update components and sintax errors
    update temperatureImpact
    update testSoil

--- produceme/src/app/test-soil/test-soil.component.ts
+++ produceme/src/app/test-soil/test-soil.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey'];
+
 
 @Component({
   selector: 'app-test-soil',
@@ -6,10 +16,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-soil.component.css']
 })
 export class TestSoilComponent implements OnInit {
+  public model: any;
 
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    );
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  
 }
